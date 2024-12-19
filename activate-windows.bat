f0dcd96fcefa00bc1df869c56afd0ee4fafcaa07
XulbuX <142539516+XulbuX-dev@users.noreply.github.com>
2024-12-19 09:35:34 +0100

    x

--- activate-windows.bat
+++ activate-windows.bat
@@ -2,18 +2,22 @@
 setlocal EnableDelayedExpansion
 title Activate Windows
 
+
+::::::::::::::::::::::: VARIABLES :::::::::::::::::::::::
+
+set "TITLE=Activate Windows"
+
 :: PRODUCT KEYS
-set "KEYS[10][Enterprise]=NPPR9-FWDCX-D2C8J-H872K-2YT43|DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4|YYVX9-NTFWV-6MDM3-9PT4T-4M68B"
-set "KEYS[10][Home]=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99|3KHY7-WNT83-DGQKR-F7HPR-844BM|7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH"
-set "KEYS[10][Education]=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2|2WH4N-8QGBV-H22JP-CT43Q-MDWWJ"
-set "KEYS[10][Pro]=W269N-WFGWX-YVC9B-4J6C9-T83GX|MH37W-N47XK-V7XM9-C7227-GCQG9|NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J"
-set "KEYS[11][Enterprise]=NPPR9-FWDCX-D2C8J-H872K-2YT43|DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4|YYVX9-NTFWV-6MDM3-9PT4T-4M68B"
-set "KEYS[11][Home]=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99|3KHY7-WNT83-DGQKR-F7HPR-844BM|7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH"
-set "KEYS[11][Education]=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2|2WH4N-8QGBV-H22JP-CT43Q-MDWWJ"
-set "KEYS[11][Pro]=W269N-WFGWX-YVC9B-4J6C9-T83GX|MH37W-N47XK-V7XM9-C7227-GCQG9|NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J"
+set "KEYS[Enterprise]=NPPR9-FWDCX-D2C8J-H872K-2YT43|DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4|YYVX9-NTFWV-6MDM3-9PT4T-4M68B"
+set "KEYS[Home]=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99|3KHY7-WNT83-DGQKR-F7HPR-844BM|7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH"
+set "KEYS[Education]=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2|2WH4N-8QGBV-H22JP-CT43Q-MDWWJ"
+set "KEYS[Pro]=W269N-WFGWX-YVC9B-4J6C9-T83GX|MH37W-N47XK-V7XM9-C7227-GCQG9|NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J"
 
 :: KMS SERVERS
 set "KMS=kms7.msguides.com|kms8.msguides.com|kms9.msguides.com"
+
+
+::::::::::::::::::::::: MAIN SCRIPT :::::::::::::::::::::::
 
 :: DETECT WINDOWS VERSION & EDITION
 for /f "tokens=2 delims==" %%a in ('wmic os get caption /value') do (
@@ -23,26 +27,82 @@ for /f "tokens=2 delims==" %%a in ('wmic os get caption /value') do (
     )
 )
 
+:: GET CONSOLE WIDTH
+for /F "usebackq tokens=2* delims=: " %%w in (`mode con ^| findstr Columns`) do set CONSOLE_WIDTH=%%w
+
+:: GET STRING LENGTH
+>%TEMP%\~stringlength.tmp echo %TITLE%
+for %%l in (%TEMP%\~stringlength.tmp) do set /A STR_LEN = %%~zl - 2
+del %TEMP%\~stringlength.tmp
+
+:: GENERATE SEPARATOR LINES
+set /A num_chars=(%CONSOLE_WIDTH% - (%STR_LEN% + 2)) / 2
+set "SEP_LINE1="
+set "SEP_LINE2="
+set "SEP_LINE_TITLE="
+for /L %%i in (1,1,%CONSOLE_WIDTH%) do set "SEP_LINE1=!SEP_LINE1!#"
+for /L %%i in (1,1,%CONSOLE_WIDTH%) do set "SEP_LINE2=!SEP_LINE2!-"
+for /L %%i in (1,1,%num_chars%) do set "SEP_LINE_TITLE=!SEP_LINE_TITLE!#"
+
 echo.
-echo 
-echo Activating Microsoft Windows %VERSION% %EDITION% ...
+call :echoTitle "%TITLE%" "#"
+echo Microsoft Windows %VERSION% %EDITION%
+echo %SEP_LINE1%
 echo.
 
 :: ACTIVATION
 set "keys=!KEYS[%EDITION%]!"
-for %%k in ("%keys:|=" "%") do (
-    echo Installing KMS client key %%~k ...
-    cscript //nologo slmgr.vbs /ipk %%~k >nul
-    echo Setting KMS machine address ...
-    cscript //nologo slmgr.vbs /skms kms8.msguides.com >nul
-    echo Activating ...
-    cscript //nologo slmgr.vbs /ato | find /i "successfully" >nul && (
-        echo Activation successful.
-        goto :EOF
+for %%s in ("%KMS:|=" "%") do (
+    for %%k in ("%keys:|=" "%") do (
+        call :echoTitle "Install KMS client key %%k" "-"
+        cscript //nologo %WINDIR%\system32\slmgr.vbs /ipk %%~k >nul 2>&1 || (
+            echo Failed to install KMS client key.
+        )
+        call :echoTitle "Set KMS machine address %%s" "-"
+        cscript //nologo %WINDIR%\system32\slmgr.vbs /skms %%~s >nul 2>&1 || (
+            echo Failed to set KMS machine address: Server might be unavailable.
+        )
+        call :echoTitle "Activate Windows" "-"
+        cscript //nologo %WINDIR%\system32\slmgr.vbs /ato > "%TEMP%\~activation.tmp" 2>&1
+        type "%TEMP%\~activation.tmp"
+        find /i "successfully" "%TEMP%\~activation.tmp" >nul && (
+            echo.
+            echo %SEP_LINE1%
+            echo Successfully activated Windows!
+            echo %SEP_LINE1%
+            goto :EOF
+        ) || (
+            echo Activation failed! Error details above.
+        )
+        echo.
     )
 )
 
-echo Activation failed. Please check your internet connection or try again later.
+echo.
+echo All activation attempts failed!
+echo Please check your internet connection or try again later.
+goto :EOF
+
+
+::::::::::::::::::::::: FUNCTIONS :::::::::::::::::::::::
+
+:echoTitle
+:: GET CONSOLE WIDTH
+for /F "usebackq tokens=2* delims=: " %%w in (`mode con ^| findstr Columns`) do set CONSOLE_WIDTH=%%w
+:: GET STRING LENGTH
+>%TEMP%\~stringlength.tmp echo %~1
+for %%l in (%TEMP%\~stringlength.tmp) do set /A STR_LEN = %%~zl - 2
+del %TEMP%\~stringlength.tmp
+:: GENERATE SEPARATOR LINES
+set /A num_chars=(%CONSOLE_WIDTH% - (%STR_LEN% + 2)) / 2
+set "SEP_LINE="
+for /L %%i in (1,1,%num_chars%) do set "SEP_LINE=!SEP_LINE!%~2"
+:: OUTPUT
+echo %SEP_LINE% %~1 %SEP_LINE%
+goto :EOF
+
+
+::::::::::::::::::::::: END :::::::::::::::::::::::
 
 :EOF
 echo.
